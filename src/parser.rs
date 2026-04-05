a94c0a1afd83395e6979a75cf436a61cb79f0c7c
Daniel Pereira <dpereira718@me.com>
2026-04-05 21:16:41 +0100

    Restore lib property value read order

--- src/parser.rs
+++ src/parser.rs
@@ -1996,6 +1996,20 @@ impl KiCadSchematicParser {
             return Err(self.error_here("Empty property name"));
         }
 
+        let value = self
+            .need_symbol_atom("property value")
+            .map_err(|_| self.error_here("Invalid property value"))
+            .map(|raw| {
+                if self.version.unwrap_or(SEXPR_SCHEMATIC_FILE_VERSION)
+                    < VERSION_EMPTY_TILDE_IS_EMPTY
+                    && raw == "~"
+                {
+                    String::new()
+                } else {
+                    raw
+                }
+            })?;
+
         let field_id = match name.to_ascii_lowercase().as_str() {
             "reference" => PropertyKind::SymbolReference,
             "value" => PropertyKind::SymbolValue,
@@ -2011,19 +2025,7 @@ impl KiCadSchematicParser {
             property.ordinal = symbol.next_field_ordinal();
         }
 
-        property.value = self
-            .need_symbol_atom("property value")
-            .map_err(|_| self.error_here("Invalid property value"))
-            .map(|raw| {
-                if self.version.unwrap_or(SEXPR_SCHEMATIC_FILE_VERSION)
-                    < VERSION_EMPTY_TILDE_IS_EMPTY
-                    && raw == "~"
-                {
-                    String::new()
-                } else {
-                    raw
-                }
-            })?;
+        property.value = value;
 
         while !self.at_right() {
             self.need_left()?;
