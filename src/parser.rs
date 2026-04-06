9ecc7ff7f67d9d9cd077009d57d433ffa7c025eb
Daniel Pereira <dpereira718@me.com>
2026-04-06 14:50:44 +0100

    Accept bare effects href heads

--- src/parser.rs
+++ src/parser.rs
@@ -5211,7 +5211,7 @@ impl KiCadSchematicParser {
             }
             let head = self.need_unquoted_symbol_atom("font, justify, hide or href")?;
 
-            if !section_is_list && head != "hide" {
+            if !section_is_list && head != "hide" && head != "href" {
                 return Err(self.expecting(")"));
             }
 
